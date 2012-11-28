c134dc7c8571d7aad64658fed8a6a24b7b871cfc
Brasegajames <brasegajames@hotmail.fr>
2012-11-29 00:44:32 +0100

    TP2 start

---
+++ Informatique/C#/TP2/Console TP2/Console TP2/Ex1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// http://www.csharpfr.com/forum/sujet-FAIRE-CALCULATRICE-AVEC-CSHARP-DEBUTANT_1370965.aspx
+
+namespace Console_TP2
+{
+    class Ex1
+    {
+
+        public Ex1()
+        {
+            string[] args = new string[] { " Nathalie ", " Krisboul ", " Xavier " };
+            int i = 0;
+            String names = "";
+            while (i < names.Length)
+            {
+                Console.WriteLine(names[i]);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Informatique/C#/TP2/Console TP2/Console TP2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex1 ex1 = new Ex1();
+            Ex2 ex2 = new Ex2();
+            Ex3 ex3 = new Ex3();
+        }
+    }
+}

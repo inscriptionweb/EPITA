ff32652bc78754af25dfde27401c774b57b48d16
brasegajames <brasegajames@hotmail.fr>
2012-11-30 11:27:52 +0100

    ex1 ex2

---
+++ Informatique/C#/TP2/Ex1/Ex1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =Int32.Parse(args[0]);
+
+            for (int i = 1; i < args.Length; i += 2)
+            {
+                if (args[i] == "*")
+                {
+                    n *= Int32.Parse(args[i+1]);
+                }
+                if (args[i] == "/")
+                {
+                    n /= Int32.Parse(args[i + 1]);
+                }
+                if (args[i] == "+")
+                {
+                    n += Int32.Parse(args[i+1]);
+                }
+                if (args[i] == "-")
+                {
+                    n -= Int32.Parse(args[i+1]);
+                }
+            }
+
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Informatique/C#/TP2/Ex2/Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String phrase = Console.ReadLine();
+
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] ==)
+                {
+                    
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}

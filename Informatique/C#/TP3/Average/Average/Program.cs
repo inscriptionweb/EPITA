2bd1f9bf2ee067fdfdb7a83031090a017a9fb033
brasegajames <brasegajames@hotmail.fr>
2012-12-14 11:03:13 +0100

    TP3 C# start!

---
+++ Informatique/C#/TP3/Average/Average/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Combien de nombres?");
+            int n = Int32.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.Write("Moyenne : ");
+                Console.WriteLine("0");
+                Console.ReadLine();
+            }
+            else
+            {
+
+                int a;
+                int tot = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Nombre ");
+                    Console.Write(i + 1);
+                    Console.WriteLine(" :");
+                    a = Int32.Parse(Console.ReadLine());
+
+                    if (a < 0)
+                    {
+                        a = 0;
+                    }
+                        
+                    tot = tot + a;
+                }
+                Console.Write("Moyenne : ");
+                Console.WriteLine(tot / n);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Informatique/C#/TP3/Maths/Maths/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Division(2, 0));
+            Console.ReadLine();
+
+        }
+
+        static int Multiplication(int a, int b)
+        {
+            int c = a;
+            for (int i = 1; i < c; i++)
+            {
+                a = a + c;
+            }
+            return a;
+        }
+
+
+        static int Division(int a, int b)
+        {
+            if (b == 0)
+                Console.WriteLine("SIRIOUS LEE? b=0?");
+            int i = 0;
+            if (a<0)
+                a = -a;
+            if (b<0)
+                b = -b;
+            while (a != 0) 
+            {
+                a = a - b;
+                i++;
+            }
+            return i;
+        }
+
+
+
+        static int Factorielle(int n)
+        {
+            int x = n;
+            for (int i = 0; i < x; i++)
+            {
+                x = x - 1;
+                n = x * n;
+            }
+
+            return n;
+        }
+
+        static int Fibonacci(int n)
+        {
+
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+
+        }
+
+        /*
+        static int FibonacciIter(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            for (int i = 0; i < c; i++)
+            {
+                n = n +  
+            }
+            return 2;
+        }
+    */
+    }
+}
